77c995e6994b7cca98b50205a6e9769046120541
Michalw101 <michal24263@gmail.com>
2024-01-02 23:29:28 +0200

    try

--- js/log-in- sign-up.js
+++ js/log-in- sign-up.js
@@ -1,3 +1,4 @@
+// try try try
 const signUpButton = document.getElementById("signUp");
 const signInButton = document.getElementById("signIn");
 const container = document.getElementById("container");
